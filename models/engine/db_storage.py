0054dd0365e4e97c3e8bf972da29191826895480
amina <amina.1111@yahoo.fr>
2024-03-29 21:51:54 +0100

    DBStorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -85,3 +85,4 @@ class DBStorage:
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
+        self.__session = Session
