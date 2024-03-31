4010623b393ae96a27612f3e1aba7e38e43530d3
sachihiroo <saif3antar@gmail.com>
2024-03-31 06:22:34 +0100

    fixed

--- models/state.py
+++ models/state.py
@@ -13,10 +13,10 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state", cascade="delete")
+    cities = relationship("City", backref="state", cascade="delete")
+    if os.getenv("HBNB_TYPE_STORAGE") != "db":
 
-    else:
+    
 
         def cities(self):
             from models import storage
