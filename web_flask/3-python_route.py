78f14c05a1864c9da0afafce282b02e67c796585
amina <amina.1111@yahoo.fr>
2024-04-09 22:35:08 +0100

    define a route for the /python/<text> URL

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Write a script that starts a Flask web application:
+Routes:
+/: display “Hello HBNB!”
+/hbnb: display “HBNB”
+/c/<text>: display “C ” followed by the value of the text variable
+/python/<text>: display “Python ”, followed by the value of the text variable
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """display Hello HBNB!"""
+    return "<p>Hello HBNB!</p>"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """Display HBNB"""
+    return "<p>HBNB</p>"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def C_is_fun(text):
+    """display C followed by the value of the text variable"""
+    string = "<p>C %s</p>" % text
+    string = string.replace("_", " ")
+    return string
+
+
+@app.route("/python", defaults={"text": "is cool"})
+@app.route("/python/<text>", strict_slashes=False)
+def Python_is_cool(text):
+    """display Python, followed by the value of text"""
+    string1 = "<p>Python %s</p>" % text
+    string1 = string1.replace("_", " ")
+    return string1
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
