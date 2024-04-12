00f6a1252756e047711e5a476038665ec12586be
amina <amina.1111@yahoo.fr>
2024-04-12 10:09:52 +0100

    define a route for the /number/<int:n> URL

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -15,36 +15,35 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_HBNB():
     """display Hello HBNB!"""
-    return "<p>Hello HBNB!</p>"
+    return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def HBNB():
     """Display HBNB"""
-    return "<p>HBNB</p>"
+    return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def C_is_fun(text):
     """display C followed by the value of the text variable"""
-    string = "<p>C %s</p>" % text
+    string = "C %s" % text
     string = string.replace("_", " ")
     return string
 
 
-@app.route("/python", defaults={"text": "is cool"})
+@app.route("/python", defaults={"text": "is cool"}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def Python_is_cool(text):
     """display Python, followed by the value of text"""
-    string1 = "<p>Python %s</p>" % text
+    string1 = f"Python {text}"
     string1 = string1.replace("_", " ")
     return string1
-
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def is_it_a_number(n):
     """display n is a number only if n is an integer"""
-    return "<p>%d is a number</p>" % n
+    return "%d is a number" % n
 
 
 if __name__ == "__main__":
